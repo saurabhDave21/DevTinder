ef33430b0c7321289dc6ae04791bfe45ce296c7d
Saurabh Dave <saurabhdave942@gmail.com>
2026-06-12 09:31:10 +0530

    SetUp node

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+
+const app = express()
+
+app.use("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+app.listen(7777,()=>{
+    console.log("Server is Start")
+})
+
